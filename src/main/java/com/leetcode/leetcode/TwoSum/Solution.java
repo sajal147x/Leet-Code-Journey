3650fbf659c88444038baf83005d34d175d87146
sajal gupta <sajal147x@gmail.com>
2026-02-02 10:06:14 -0600

    twosum

---
+++ src/main/java/com/leetcode/leetcode/TwoSum/Solution.java
@@ -0,0 +1,28 @@
+package com.leetcode.leetcode.TwoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author sajalgupta
+ */
+
+
+class Solution {
+
+    // return indices of numbers that add up to the target, only one pair in list
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> pairIdx = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (pairIdx.containsKey(target - num)) {
+                return new int[] { i, pairIdx.get(target - num) };
+            }
+            pairIdx.put(num, i);
+        }
+
+        return new int[] {};        
+    }
+    
+}
